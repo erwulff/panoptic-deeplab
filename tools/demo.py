f39ad8c2a52bc74d8bee3353fb3df8692d03925b
Eric Wulff <ericwu@axis.com>
2021-01-14 15:19:26 +0100

    Add code comments with class names in colormap
    Change-Id: Id0e32f8924e999c2a2fc48df7afa90085ea8f48e

--- tools/demo.py
+++ tools/demo.py
@@ -105,25 +105,25 @@ class CityscapesMeta(object):
             A colormap for visualizing segmentation results.
         """
         colormap = np.zeros((256, 3), dtype=np.uint8)
-        colormap[0] = [128, 64, 128]
-        colormap[1] = [244, 35, 232]
-        colormap[2] = [70, 70, 70]
-        colormap[3] = [102, 102, 156]
-        colormap[4] = [190, 153, 153]
-        colormap[5] = [153, 153, 153]
-        colormap[6] = [250, 170, 30]
-        colormap[7] = [220, 220, 0]
-        colormap[8] = [107, 142, 35]
-        colormap[9] = [152, 251, 152]
-        colormap[10] = [70, 130, 180]
-        colormap[11] = [220, 20, 60]
-        colormap[12] = [255, 0, 0]
-        colormap[13] = [0, 0, 142]
-        colormap[14] = [0, 0, 70]
-        colormap[15] = [0, 60, 100]
-        colormap[16] = [0, 80, 100]
-        colormap[17] = [0, 0, 230]
-        colormap[18] = [119, 11, 32]
+        colormap[0] = [128, 64, 128]  # road
+        colormap[1] = [244, 35, 232]  # sidewalk
+        colormap[2] = [70, 70, 70]  # building
+        colormap[3] = [102, 102, 156]  # wall
+        colormap[4] = [190, 153, 153]  # fence
+        colormap[5] = [153, 153, 153]  # pole
+        colormap[6] = [250, 170, 30]  # traffic light
+        colormap[7] = [220, 220, 0]  # traffic sign
+        colormap[8] = [107, 142, 35]  # vegetation
+        colormap[9] = [152, 251, 152]  # terrain
+        colormap[10] = [70, 130, 180]  # sky
+        colormap[11] = [220, 20, 60]  # person
+        colormap[12] = [255, 0, 0]  # rider
+        colormap[13] = [0, 0, 142]  # car
+        colormap[14] = [0, 0, 70]  # truck
+        colormap[15] = [0, 60, 100]  # bus
+        colormap[16] = [0, 80, 100]  # train
+        colormap[17] = [0, 0, 230]  # motorcycle
+        colormap[18] = [119, 11, 32]  # bicycle
         return colormap
 
 
